2a9ab1c3cfed9971cd5a9a45f35b39b3d07a84ad
Max Zolotkov <maxim.zolotkov@yandex.ru>
2023-06-30 15:47:54 +0300

    chore: add live reload if scss is changed

--- card-gallery/config/webpack.dev.config.js
+++ card-gallery/config/webpack.dev.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const html = path.resolve(__dirname, '../src/**/*.html');
+const scss = path.resolve(__dirname, '../src/**/*.scss');
 
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    watchFiles: [path.resolve(__dirname, '../src/**/*.html')],
+    watchFiles: [html, scss],
   },
 };
