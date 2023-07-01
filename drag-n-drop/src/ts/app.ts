d982caacba95e4457242cc69a6076216b47c6b33
Max Zolotkov <maxim.zolotkov@yandex.ru>
2023-07-01 15:13:42 +0300

    feat: add update counters function

--- drag-n-drop/src/ts/app.ts
+++ drag-n-drop/src/ts/app.ts
@@ -1,1 +1,15 @@
 import '../scss/style.scss';
+import { createTask } from './files/task';
+import { updateTasksCounters } from './files/functions';
+
+function start() {
+  const createTaskButton = document.querySelector('.todo__create')!;
+  const input: HTMLInputElement = document.querySelector('.todo__input')!;
+
+  createTaskButton.addEventListener('click', createTask);
+  input.addEventListener('focus', () => (input.placeholder = ''));
+
+  updateTasksCounters();
+}
+
+window.onload = () => start();
