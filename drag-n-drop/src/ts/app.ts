1b41d2e29cf6201a3187d48442573f2e8435e0c6
FreightDH <maxim.zolotkov@yandex.ru>
2023-07-01 19:53:25 +0300

    feat: add normal drag-n-drop functions

--- drag-n-drop/src/ts/app.ts
+++ drag-n-drop/src/ts/app.ts
@@ -1,69 +1,19 @@
 import '../scss/style.scss';
 import { createTask } from './files/task';
-import { updateTasksCounters } from './files/functions';
+import { setDragDropEvents, updateTasksCounters } from './files/functions';
 
-function start() {
+function start(): void {
   const createTaskButton = document.querySelector('.todo__create')!;
   const input: HTMLInputElement = document.querySelector('.todo__input')!;
-  const rowWithTasks = document.querySelector('.row--tasks')!;
-  const tasksPlaceholders = document.querySelectorAll('.todo__placeholder');
 
   createTaskButton.addEventListener('click', createTask);
+
   input.addEventListener('focus', () => (input.placeholder = ''));
-
-  rowWithTasks.addEventListener('dragstart', (event) => {
-    const task = event.target as HTMLElement;
-    task.classList.add('hold');
-    setTimeout(() => task.classList.add('hide'), 0);
-
-    tasksPlaceholders.forEach((placeholder) => {
-      placeholder.addEventListener('dragover', (event) => event.preventDefault());
-      placeholder.addEventListener('dragenter', (event) => {
-        placeholder.classList.add('selected');
-      });
-      placeholder.addEventListener('dragleave', (event) => {
-        placeholder.classList.remove('selected');
-      });
-    });
+  input.addEventListener('keyup', (event) => {
+    if (event.code === 'Enter') createTask();
   });
 
-  rowWithTasks.addEventListener('dragend', (event) => {
-    const task = event.target as HTMLElement;
-    task.classList.remove('hold');
-    task.classList.remove('hide');
-
-    tasksPlaceholders.forEach((placeholder) => {
-      placeholder.addEventListener('drop', (event) => {
-        placeholder.classList.remove('selected');
-        placeholder.appendChild(task);
-        updateTasksCounters();
-      });
-    });
-  });
-
-  // rowWithTasks.addEventListener('dragenter', (event) => {
-  //   const target = event.target as HTMLElement;
-  //   target.classList.add('selected');
-  // });
-
-  // tasksPlaceholders.forEach((placeholder) => {
-  //   placeholder.addEventListener('dragover', (event) => event.preventDefault());
-  //   placeholder.addEventListener('dragenter', (event) => {
-  //     const target = event.target as HTMLElement;
-  //     console.log(target);
-  //     target.classList.add('selected');
-  //   });
-  //   placeholder.addEventListener('dragleave', (event) => {
-  //     const target = event.target as HTMLElement;
-  //     target.classList.remove('selected');
-  //   });
-  //   placeholder.addEventListener('drop', (event) => {
-  //     const target = event.target as HTMLElement;
-  //     target.classList.remove('selected');
-  //     // target.appendChild(item);
-  //   });
-  // });
-
+  setDragDropEvents();
   updateTasksCounters();
 }
 
